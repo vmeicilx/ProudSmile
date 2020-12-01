792bd46449049f0ecc18b615754a7a7b5b83f44b
valexand-tibco <valexand@tibco.com>
2020-12-01 11:54:58 +0200

    1 dec 2020

--- src/app/pages/why-proud-smile/dental-clinic/dental-clinic.component.ts
+++ src/app/pages/why-proud-smile/dental-clinic/dental-clinic.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-dental-clinic",
-  templateUrl: "./dental-clinic.component.html"
+  templateUrl: "./dental-clinic.component.html",
+  styleUrls: ["./dental-clinic.component.scss"]
 })
 export class DentalClinicComponent implements OnInit {
   constructor() {}
--- src/app/pages/why-proud-smile/proud-smile-experience/proud-smile-experience.component.ts
+++ src/app/pages/why-proud-smile/proud-smile-experience/proud-smile-experience.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-proud-smile-experience",
@@ -6,7 +7,27 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./proud-smile-experience.component.scss"]
 })
 export class ProudSmileExperienceComponent implements OnInit {
-  constructor() {}
+  @ViewChild("sound1") sound1: ElementRef;
+  @ViewChild("soundButton1") soundButton1: ElementRef;
+  @ViewChild("sound2") sound2: ElementRef;
+  @ViewChild("soundButton2") soundButton2: ElementRef;
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  playSound1() {
+    this.soundButton1.nativeElement.style.animationPlayState = "paused";
+    let sound1 = this.sound1.nativeElement;
+    sound1.play();
+  }
+  playSound2() {
+    this.soundButton2.nativeElement.style.animationPlayState = "paused";
+    let sound2 = this.sound2.nativeElement;
+    sound2.play();
+  }
+
+  seeAllFinancial() {
+    this.router.navigate(["/", "payment-page-component"]);
+  }
 }
