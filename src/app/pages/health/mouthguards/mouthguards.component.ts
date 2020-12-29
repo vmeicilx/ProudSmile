e78690ee333c41df1840c65de3b105d970184d03
valexand-tibco <valexand@tibco.com>
2020-12-29 22:08:27 +0200

    29dec

--- src/app/pages/health/mouthguards/mouthguards.component.ts
+++ src/app/pages/health/mouthguards/mouthguards.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-mouthguards",
-  templateUrl: "./mouthguards.component.html"
+  templateUrl: "./mouthguards.component.html",
+  styleUrls: ["./mouthguards.component.scss"]
 })
 export class MouthguardsComponent implements OnInit {
   constructor() {}
--- src/app/pages/health/same-day-crowns/same-day-crowns.component.ts
+++ src/app/pages/health/same-day-crowns/same-day-crowns.component.ts
@@ -1,11 +1,67 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild
+} from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-same-day-crowns",
-  templateUrl: "./same-day-crowns.component.html"
+  templateUrl: "./same-day-crowns.component.html",
+  styleUrls: ["./same-day-crowns.component.scss"]
 })
 export class SameDayCrownsComponent implements OnInit {
-  constructor() {}
+  @ViewChild("cerec") cerecVideo: ElementRef;
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.cerecVideo.nativeElement.muted = true;
+    this.cerecVideo.nativeElement.style.display = "none";
+    this.cerecVideo.nativeElement.onfullscreenchange = this.fullscreenChangeHandler;
+  }
+
+  @HostListener("window:scroll", ["$event"]) // for window scroll events
+  onScroll(event) {}
+
+  inTheViewport(elem): boolean {
+    var bounding = elem.getBoundingClientRect();
+    if (
+      bounding.top >= -elem.offsetHeight &&
+      bounding.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) +
+          elem.offsetHeight
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  fullscreenChangeHandler(event) {
+    let elem = event.target;
+    let isFullscreen = document.fullscreenElement === elem;
+    if (!isFullscreen) {
+      elem.style.display = "none";
+    }
+  }
+
+  playVideo() {
+    let video = this.cerecVideo.nativeElement;
+    video.style.display = "block";
+    video.setAttribute("controls", "controls");
+    video.requestFullscreen();
+    video.play();
+  }
+
+  seeAllFinancial() {
+    this.router.navigate(["/", "payment-page-component"]);
+  }
+  goToLink(url: string) {
+    window.open(url, "_blank");
+  }
 }
