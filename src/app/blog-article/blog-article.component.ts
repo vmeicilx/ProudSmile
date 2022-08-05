1f3226871c0995e7bc0707face6fcdb125022135
Vlad Alexandru Meici <vlad.meici@raro.eu>
2022-08-05 11:23:45 +0300

    blabla

--- src/app/blog-article/blog-article.component.ts
+++ src/app/blog-article/blog-article.component.ts
@@ -15,8 +15,6 @@ export class BlogArticleComponent implements OnInit {
   error = null;
   api_url = "https://proudsmileblog-app-tdioj.ondigitalocean.app";
 
-  
-
   constructor(private _activatedRoute: ActivatedRoute, private router: Router) { }
 
   async ngOnInit() {
@@ -27,31 +25,14 @@ export class BlogArticleComponent implements OnInit {
 
     try {
 
-      // const { data } = await axios.post('http://localhost:1337/api/auth/local', {
-      //   identifier: 'vlad.meici@gmail.com',
-      //   password: '!Alecsandru3',
-      // });
-
       //const response = await axios.get('https://proudsmileblog-app-tdioj.ondigitalocean.app/api/posts');
-      const response = await axios.get('http://localhost:1337/api/posts/' + this.articleID + '?populate=*', {
+      const response = await axios.get(this.api_url + '/api/posts/' + this.articleID + '?populate=*', {
         headers: {
           Authorization:
-          'Bearer a438f488efd591a865bb2ccbde898dbab64daabb8fe2ccfd3195d88d939d47ef0f9ac5a4442b2e300f58f06eb0455d594ed55d4df111a180adbc19fa2a47f522ecadb7d8c089574fe461035e9c41bd1150ac4b794acecd53163adb1bc6b9c15fe5f3ee15e3a059e5b8e9c5909ec2b206cb195b3b9154499a01aa175b1f68074a',
+          'Bearer b2afceb61de2072a688b5cc7be86131eeb940fdbff0aca5ade11c65376a047ee2e97c1af3ec8a3a1a860d9b588339677ae2dce9c4dea23226e3377ecd1edd8ee328a22fb9a0f1c7bffebb6acbbee87246320109326cc6eedb6f0c7e44f6b7f76f346d9ee93879eba75eddc9bacfdcf34bfc8932fd549f631cb0f64e4bcbedf59',
         },
       });
       this.post = response.data.data;
-
-      // rtfToHtml.fromString(this.post.attributes.description, (err, html) => {
-      //   console.log(html)
-      //   // prints a document containing:
-      //   // <p><strong>hi there</strong></p>
-      // })
-      
-      // rtfToHTML.fromString(this.post.attributes.article, (err, html) => {
-      //   console.log(html)
-      //   // prints a document containing:
-      //   // <p><strong>hi there</strong></p>
-      // })
 
     } catch (error) {
       this.error = error;
--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -63,40 +63,46 @@ export class BlogComponent implements OnInit {
           'Bearer b2afceb61de2072a688b5cc7be86131eeb940fdbff0aca5ade11c65376a047ee2e97c1af3ec8a3a1a860d9b588339677ae2dce9c4dea23226e3377ecd1edd8ee328a22fb9a0f1c7bffebb6acbbee87246320109326cc6eedb6f0c7e44f6b7f76f346d9ee93879eba75eddc9bacfdcf34bfc8932fd549f631cb0f64e4bcbedf59',
         },
       });
+      categoriesResponse.data.data.sort((a,b) => a.id > b.id ? 1 : -1);
       this.categories = this.categories.concat(categoriesResponse.data.data);
 
       this.numberOfPages = this.posts.length / this.articlePerPage;
       this.filteredPosts = this.posts;
       this.filteredPosts = this.filteredPosts.slice((this.currentPage - 1) * this.articlePerPage, this.currentPage * this.articlePerPage);
+      
+      this.filteredPosts.sort((a,b) => a.id > b.id ? 1 : -1);
       this.currentCategory = "All";
       
       this.setDisplayPages();
       
-      console.log(this.categories)
     } catch (error) {
       this.error = error;
     }
 
   }
 
-  ngAfetrViewInit(): void {
+  ngAfterViewInit(): void {
     (
       document.documentElement.getElementsByTagName(
         "app-blog"
       )[0] as HTMLElement
     ).style.overflow = "hidden";
+
+    setTimeout(() => {
+      this.magicGrid = new MagicGrid({
+        container: "#container",
+        items: this.filteredPosts.length,
+        animate: true
+      });
+      this.magicGrid.listen();
+      this.magicGrid.positionItems();
+  }, 300)
   }
 
   @HostListener('window:load')
   onLoad() {
 
-    this.magicGrid = new MagicGrid({
-      container: "#container", // Required. Can be a class, id, or an HTMLElement.
-      items: this.filteredPosts.length, // For a grid with 20 items. Required for dynamic content.
-      animate: true, // Optional.,
-    });
-
-    this.magicGrid.listen();
+    
   }
 
   onArticlePage(articleId: string) {
@@ -119,6 +125,7 @@ export class BlogComponent implements OnInit {
     this.numberOfPages = this.filteredPosts.length / this.articlePerPage;
     this.filteredPosts = this.filteredPosts.slice((this.currentPage - 1) * this.articlePerPage, this.currentPage * this.articlePerPage);
     
+    this.filteredPosts.sort((a,b) => a.id > b.id ? 1 : -1);
     this.setDisplayPages();
 
     setTimeout(() => {
