775635b9751f6a42ed67925c70365c1100806465
Meici Vlad Alexandru <vmeici@live.com>
2023-01-15 10:05:40 +0200

    Add images versions for mobile on home page. Remove unused code on home page.

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -4,7 +4,6 @@ import {
   ElementRef,
   Renderer2,
   ViewChild,
-  HostListener,
 } from "@angular/core";
 
 import { Router } from "@angular/router";
@@ -22,104 +21,13 @@ function resize() {
 
 window.onresize = resize;
 
-function startAnimation() {
-  const canvas: any = document.getElementById("hero-proudsmile");
-  const context = canvas.getContext("2d");
-  document.body.style.overflow = "hidden";
-
-  let startValue = "top 129px";
-  let endValue = "+=5000";
-
-  if(window.innerWidth < 1200) {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerWidth / (1364 / 700);
-    startValue = "top 80px";
-  }
-  else
-  {
-    canvas.width = 1364;
-    canvas.height = 700;
-  }
-
-
-  const frameCount = 200;
-  const currentFrame = (index) =>
-    `../../../../assets/homePage/short/Porcelain Veneers Short ${(
-      index + 1
-    )
-      .toString()
-      .padStart(3, "0")}.jpg`;
-
-  const images = [];
-  const frames = {
-    frame: 0,
-  };
-
-  let loadedImages = 0;
-
-  for (let i = 0; i < frameCount; i++) {
-    const img = new Image();
-    img.src = currentFrame(i);
-    img.onload = countImages;
-    
-    if(window.innerWidth < 1200) {
-      img.width = window.innerWidth;
-      img.height = window.innerWidth / (1364 / 700);
-    }
-    images.push(img);
-  }
-
-  gsap.to(frames, {
-    frame: frameCount - 1,
-    snap: "frame",
-    scrollTrigger: {
-      id: "ScrollTriggerProudSmile",
-      trigger: "#ProudSmileParent",
-      start: startValue,
-      end: endValue,
-      markers: false,
-      scrub: 1,
-      pin: true
-    },
-    onUpdate: render,
-  });
-
-  function countImages() {
-    loadedImages = loadedImages + 1;
-    if (loadedImages === frameCount) {
-      render();
-      ScrollTrigger.refresh();
-    }
-  }
-
-  function render() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(images[frames.frame], 0, 0, images[frames.frame].width, images[frames.frame].height, 0, 0, canvas.width, canvas.height);
-    resize();
-    const canvasParent: any = document.getElementById("ProudSmileParent");
-    
-    if(canvasParent) {
-      if(window.innerWidth < 1200) {
-        canvasParent.style.top = "80px";
-      }
-      else
-      {
-        canvasParent.style.top = "129px";
-      }
-    }
-  }
-}
-
 @Component({
   selector: "app-home-page",
   templateUrl: "./home-page.component.html",
   styleUrls: ["./home-page.component.scss"],
 })
 export class HomePageComponent implements OnInit {
-  private elRef: ElementRef;
-  private renderer: Renderer2;
 
-  @ViewChild("relaxVideo") relaxVideo: ElementRef;
   @ViewChild("vennerShortText") vennerShortText: ElementRef;
   @ViewChild("vennerShortTextMobile") vennerShortTextMobile: ElementRef;
 
@@ -198,48 +106,11 @@ export class HomePageComponent implements OnInit {
       name: 'description',
       content: `The dentists you'll actually want to visit. Modern general dentistry and bespoke aesthetic treatments on the Gold Coast - Bundall, Pacific Fair.`
     }, { name: 'keywords', content: 'dentist gold coast, dental clinic gold coast, dental care center' }]);
-    this.elRef = el;
-    this.renderer = renderer;
   }
 
   ngOnInit() {
   }
 
-  ngAfterViewInit(): void {
-    this.relaxVideo.nativeElement.muted = true;
-    this.relaxVideo.nativeElement.style.display = "block";
-
-    if (window.innerWidth <= 1200) {
-      this.relaxVideo.nativeElement.autoplay = true;
-    }
-
-    
-    if (window.innerWidth > 1200) {
-
-    // setTimeout(() => {
-    //   startAnimation();
-    //   resize();
-    // }, 100);
-  }
-
-  }
-
-  @HostListener("window:scroll", ["$event"]) // for window scroll events
-  onScroll(event) {
-    let playPromise;
-    if (ScrollTrigger.isInViewport(this.relaxVideo.nativeElement)) {
-      playPromise = this.relaxVideo.nativeElement.play();
-    }
-    else {
-      if (playPromise !== undefined) {
-        playPromise.then(_ => {
-          this.relaxVideo.nativeElement.pause();
-        })
-          .catch(error => {
-          });
-      }
-    }
-  }
   showTextPre($event) {
     if (this.vennerShortText) {
       this.vennerShortText.nativeElement.style.position = "absolute";
